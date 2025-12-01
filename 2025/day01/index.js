98b32e131ac0edb3777c94838c9397e9c7d0204b
Hristo Karamanliev <hkaramanliev@despark.com>
2025-12-02 01:02:23 +0200

    quick & dirty rotateDial func

--- 2025/day01/index.js
+++ 2025/day01/index.js
@@ -1,15 +1,45 @@
 import { readFile } from "fs/promises";
 
 const input = await readFile("input", "utf8");
-
 const arrInput = input.split("\n");
-const dialArr = Array.from({ length: 99 }, (_, index) => index + 1);
-let currentStep = dialArr[49];
+
+let position = rotateDial(50, 68, "L");
+console.log(position);
 
 arrInput.forEach((step) => {
-  if (step.startsWith("L")) {
+  const distance = step.slice(1);
+
+  if (step.slice(0, 1) === "L") {
     // console.log(step);
   } else {
     // console.log(step);
   }
 });
+
+function rotateDial(currPos, distance, direction) {
+  const dialLength = 100;
+  const dialArr = Array.from({ length: dialLength }, (_, i) => i);
+  let newPosition;
+
+  if (!distance || !direction) newPosition = dialArr[currPos];
+
+  if (direction === "L") {
+    const beforeArrStart = currPos - distance < 0;
+    const newPos = beforeArrStart
+      ? dialLength - (distance - currPos)
+      : currPos - distance;
+
+    newPosition = dialArr[newPos];
+  }
+
+  if (direction === "R") {
+    const afterArrEnd = currPos + distance > dialLength - 1;
+    const newPos = afterArrEnd
+      ? distance - (dialLength - currPos)
+      : currPos + distance;
+
+    newPosition = dialArr[newPos];
+  }
+
+  return newPosition;
+}
